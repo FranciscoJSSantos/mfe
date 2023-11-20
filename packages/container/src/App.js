49722a52576ff253e53c5c683ff345136388bb3d
FRANCISCO JOSE DOS SANTOS SANTANA <francisco.a.santana@capgmemini.com>
2023-11-20 16:25:44 -0300

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MarketingReactComponent from './components/MarketingApp';
+
+export default () => {
+   return <div>
+      <h1>hey</h1>
+      <hr />
+      <MarketingReactComponent/>
+   </div>
+};
